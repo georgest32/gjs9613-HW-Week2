dfef77844327dd6bbd0bf7ef87bc5b8bb2ac51f5
george <georgest32@gmail.com>
2021-02-16 15:17:17 -1000

    added unity project

---
+++ gjs9613-week2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject player;
+    public TextMesh text;
+    public int score = 0;
+    public int targetScore = 1;
+    private int currentLvl = 1;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else Destroy(gameObject);
+        
+        //find and arrange all barriers
+        GameObject[] barriers = GameObject.FindGameObjectsWithTag("Barrier");
+
+        for (int i = 0; i < barriers.Length; i++)
+        {
+            float rand = Random.Range(1, 3);
+Debug.Log(rand);
+            //fancy ternary oo la la (if rand is greater than 0.5, yPos is -14, otherwise it's -16)
+            int yPos = rand >= 2 ? -14 : -16;
+            barriers[i].transform.localPosition = new Vector2(Random.Range(-71, -73) - i*3, yPos);
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.localPosition = new Vector2(10.26f, 0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Score: " + score;
+
+        if (score >= targetScore || SceneManager.GetActiveScene().name == "3")
+        {
+            currentLvl++;
+            targetScore++;
+            Debug.Log(currentLvl);
+            SceneManager.LoadScene(currentLvl.ToString());
+        }
+    }
+}
---
+++ gjs9613-week2/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float forceAmt =  10;
+
+    public static PlayerControl instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else Destroy(gameObject);
+
+        transform.position = new Vector2(10.26f, 0f);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.AddForce(Vector2.up * forceAmt);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddForce(Vector2.left * forceAmt);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb2d.AddForce(Vector2.down * forceAmt);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddForce(Vector2.right * forceAmt);
+        }
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * (2 * forceAmt));
+        }
+
+        //need to get 0 of camera space instead of world space
+        if (transform.position.x <= 0 || transform.position.x >= Screen.width || transform.position.y <= 0 || transform.position.y >= Screen.height)
+        {
+            //rb2d.velocity = Vector2.zero;
+        }
+    }
+}
